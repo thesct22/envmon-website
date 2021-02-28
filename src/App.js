59d1f523451ee51e397896963a8268d2bea28465
thesct22 <sharathct22@gmail.com>
2021-02-28 12:12:24 +0530

    Added firebase database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect,Component } from "react";
+
 import {ThemeProvider} from "styled-components";
 import  {useDarkMode} from "./components/useDarkMode"
 import { GlobalStyles } from "./components/globalStyles";
 import { lightTheme, darkTheme } from "./components/Themes"
-import Toggle from "./components/Toggler"
+import Toggle from "./components/Toggler";
+
+import config from './firebaseConfig';
+import firebase from "firebase/app";
+import "firebase/database";
+import {FirebaseDatabaseProvider, FirebaseDatabaseNode} from "@react-firebase/database";
+
 import LineChart from 'react-linechart';
 import '../node_modules/react-linechart/dist/styles.css';
+
 import "./App.css";
 
 const App = () => {
+  const [firebaseconfig,setconfig]=useState(config);
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseconfig);
+ }
   const [theme, themeToggler, mountedComponent] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
   const data = [
@@ -21,19 +33,34 @@ const App = () => {
       points: [{x: 1, y: 23}, {x: 3, y: 23}, {x: 7, y: 24},{x: 8, y: 25}] 
   }
 ];
+
   if(!mountedComponent) return <div/>
   return (
     <ThemeProvider theme={themeMode}>
       <>
       <GlobalStyles/>
         <div className="App">
-          <Toggle theme={theme} toggleTheme={themeToggler} />
-          <h4>Hello World</h4>
-          <LineChart 
-                        width={600}
-                        height={400}
-                        data={data}
-                    />
+          <FirebaseDatabaseProvider firebase={firebase} {...firebaseconfig}>
+            <Toggle theme={theme} toggleTheme={themeToggler} />
+            <h4>Hello World</h4>
+            <LineChart 
+              width={600}
+              height={400}
+              data={data}
+            />
+            <FirebaseDatabaseNode
+            path="hum/"
+            orderByKey
+            // orderByValue={"created_on"}
+          >
+            {d=>{
+              console.log(d);
+              return(
+                null
+              )
+            }}
+          </FirebaseDatabaseNode>
+          </FirebaseDatabaseProvider>
         </div>
       </>
     </ThemeProvider>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCRmEPexDjNFxE3IqvIW0eRSkFX63hPdes",
+    authDomain: "envmon-12b11.firebaseapp.com",
+    databaseURL: "https://envmon-12b11-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "envmon-12b11",
+    storageBucket: "envmon-12b11.appspot.com",
+    messagingSenderId: "553540257115",
+    appId: "1:553540257115:web:b57d57b85ce73f6cbc467e",
+    measurementId: "G-VQYB8MCVKW"
+};
+export default config;
