ad042f45919ce0c802f9e94d83a26f9e1ecb61c6
thesct22 <sharathct22@gmail.com>
2021-04-23 16:27:21 +0530

    Toggle for Temperature and Humidity graph done

--- src/Main.js
+++ src/Main.js
@@ -6,10 +6,11 @@ import { GlobalStyles } from "./components/globalStyles";
 import { lightTheme, darkTheme } from "./components/Themes"
 import Toggle from "./components/Toggler";
 
+import ToggleButton from './components/ToggleButton'
 
 import config from './components/firebaseConfig';
 import firebase from 'firebase/app';
-//import './App.css'
+import './App.css'
 import 'firebase/database';
 import {FirebaseDatabaseProvider, FirebaseDatabaseNode} from "@react-firebase/database";
 
@@ -20,6 +21,8 @@ import '../node_modules/react-linechart/dist/styles.css';
 const Main =({handleLogout},{handleSignUp})=> {
 
   const [firebaseconfig,setconfig]=useState(config);
+  const [humtemp, sethumtemp] = useState("/temp");
+
 //   if (!firebase.apps.length) {
 //     firebase.initializeApp(firebaseconfig);
 //  }
@@ -46,7 +49,10 @@ const Main =({handleLogout},{handleSignUp})=> {
               <a href="#">Register</a>
             </div>
           </div>
+
           <Toggle theme={theme} toggleTheme={themeToggler} />
+          <ToggleButton  daynight={false} icons={{checked: "🌡", unchecked: "💦"}} sethumtemp={sethumtemp} defaultChecked={true}/>
+
           <button onClick={handleLogout}>Logout</button>
           
         </nav>
@@ -58,9 +64,9 @@ const Main =({handleLogout},{handleSignUp})=> {
         <div className="App">
           <FirebaseDatabaseProvider firebase={firebase} {...firebaseconfig}>
             
-            <h4>Temperature</h4>
+            <h4>{humtemp==="/temp"?"Temperature":"Humidity"}</h4>
             <FirebaseDatabaseNode
-              path="temp/"
+              path={humtemp}
               orderByKey
               // orderByValue={"created_on"}
             >
@@ -73,8 +79,8 @@ const Main =({handleLogout},{handleSignUp})=> {
                     var plot1=Object.keys(d.value[keyName]).map(function(hum, humIndex) {
                       var obj={x:null,y:null};
                       var milliseconds=Number(hum);
-                      obj.y = new Date(milliseconds);
-                      obj.x=d.value[keyName][hum];
+                      obj.x = new Date(milliseconds);
+                      obj.y=d.value[keyName][hum];
                       return obj;
                     });
                     var temppoint={points:null};
@@ -94,45 +100,6 @@ const Main =({handleLogout},{handleSignUp})=> {
                 else return null
                 
               }}
-          </FirebaseDatabaseNode>
-          </FirebaseDatabaseProvider>
-          <hr></hr>
-          <h4>Humidity</h4>
-          <FirebaseDatabaseProvider firebase={firebase} {...firebaseconfig}>
-          <FirebaseDatabaseNode
-            path="hum/"
-            orderByKey
-            // orderByValue={"created_on"}
-          >
-            {d=>{
-              if(d.value!=null)
-              {
-              
-                var temppoints=Object.keys(d.value).map(function(keyName, keyIndex) {
-                  
-                  var plot1=Object.keys(d.value[keyName]).map(function(hum, humIndex) {
-                    var obj={x:null,y:null};
-                    obj.x=Number(hum);
-                    obj.y=d.value[keyName][hum];
-                    return obj;
-                  });
-                  var temppoint={points:null};
-                  temppoint.points=plot1;
-                  return temppoint;
-                });
-                console.log(temppoints);
-                return(
-                
-                <LineChart 
-                  width={600}
-                  height={400}
-                  data={temppoints}
-                />
-              )
-              }
-              else return null
-              
-            }}
           </FirebaseDatabaseNode>
           </FirebaseDatabaseProvider>
         </div>
