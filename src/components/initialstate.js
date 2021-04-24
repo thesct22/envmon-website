b2d4618370b84fb0d660c552b29a1585714a5dba
thesct22 <sharathct22@gmail.com>
2021-04-25 00:28:12 +0530

    Made main more readable

---
+++ src/components/initialstate.js
@@ -0,0 +1,13 @@
+var initialState= {
+    "Control Room":true,
+    "Conveyer Belt 1": true,
+    "Conveyer Belt 2": true,
+    "Manager's Cabin": true,
+    "Office Central": true,
+    "Office Corner 1": true,
+    "Office Corner 2": true,
+    "Office Corner 3": true,
+    "Office Corner 4": true,
+    "Storage Room": true
+};
+export default initialState;
