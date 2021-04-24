c254b16c88dce74a62c7dba618d21a75b49e2bda
thesct22 <sharathct22@gmail.com>
2021-04-25 00:33:52 +0530

    Uncommented components

--- src/App.js
+++ src/App.js
@@ -42,6 +42,8 @@ const App = ()=> {
           case "auth/wrong-password":
             setPasswordError(err.message);
             break;
+          default:
+            break;
         }
       });
   };
@@ -60,6 +62,8 @@ const App = ()=> {
           case "auth/weak-password":
             setPasswordError(err.message);
             break;
+          default:
+              break;
         }
       });
   };
